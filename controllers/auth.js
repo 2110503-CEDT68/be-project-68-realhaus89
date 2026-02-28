f2c0bfa686d26ac443c1bd6f3e0168de97554f4b
Bharxn <baozaa49@gmail.com>
2026-03-01 02:37:23 +0700

    feat: add user registration controller

---
+++ controllers/auth.js
@@ -0,0 +1,44 @@
+const User = require('../models/User');
+
+// Get token from model, create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
+    // Create token
+    const token = user.getSignedJwtToken();
+
+    const options = {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+        httpOnly: true
+    };
+
+    if (process.env.NODE_ENV === 'production') {
+        options.secure = true;
+    }
+
+    res.status(statusCode).cookie('token', token, options).json({
+        success: true,
+        token
+    });
+};
+
+// @desc    Register user
+// @route   POST /api/v1/auth/register
+// @access  Public
+exports.register = async (req, res, next) => {
+    try {
+        const { name, email, phone, password, role } = req.body;
+
+        // Create user
+        const user = await User.create({
+            name,
+            email,
+            phone,
+            password,
+            role
+        });
+
+        sendTokenResponse(user, 200, res);
+    } catch (err) {
+        res.status(400).json({ success: false });
+        console.log(err.stack);
+    }
+};
