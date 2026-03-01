351620c8b322849475cd2601d049ae71ab5fb9f7
Bharxn <baozaa49@gmail.com>
2026-03-01 18:19:30 +0700

    feat: add updateUser controller to update user by id for admin

--- controllers/users.js
+++ controllers/users.js
@@ -45,3 +45,32 @@ exports.getUser = async (req, res, next) => {
         });
     }
 };
+
+// @desc    Update user
+// @route   PUT /api/v1/users/:id
+// @access  Private
+exports.updateUser = async (req, res, next) => {
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: `User not found with id of ${req.params.id}`
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            data: user
+        });
+    } catch (err) {
+        res.status(400).json({
+            success: false,
+            message: err.message
+        });
+    }
+};
